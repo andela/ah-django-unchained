a81ad4f4a27efb158f43a3b7b16d691d8dcc5ffd
Andrew Hinga <andrewhinga5@gmail.com>
2019-01-07 16:52:27 +0300

    [Chore #162948832] Configure postgres database as db engine

--- authors/settings.py
+++ authors/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'auths_heaven',
+        'USER': 'postgres',
+        'PASSWORD': 'DB_PASS',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
