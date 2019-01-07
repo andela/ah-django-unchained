c620e984fb41d7894bf73edb74c5fba0fb2c1d73
Andrew Hinga <andrewhinga5@gmail.com>
2019-01-07 23:03:08 +0300

    [Chore #162948832] Put db variables in .env and update changes in settings

--- authors/settings.py
+++ authors/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'auths_heaven',
-        'USER': 'postgres',
-        'PASSWORD': 'DB_PASS',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv(DB_NAME),
+        'USER': os.getenv(DB_USER),
+        'PASSWORD': os.getenv(DB_PASS),
+        'HOST': os.getenv(DB_HOST),
+        'PORT': os.getenv(DB_PORT),
 
     }
 }
