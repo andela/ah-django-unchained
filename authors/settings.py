edaa297ed0a8c0def345ffe54e655ac1be18c71e
Margaret Chege <ciku@MacBook-Pro-4.local>
2019-01-16 17:53:32 +0300

    Made changes to settings file

--- authors/settings.py
+++ authors/settings.py
@@ -172,7 +172,7 @@ SWAGGER_SETTINGS = {
 }
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.getenv('SECRET_KEY')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
