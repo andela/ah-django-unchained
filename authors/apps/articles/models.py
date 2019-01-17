b6bf8db37be6ed8f21e77e3cd7b1f657bb105124
KMaina <maina.ken0@gmail.com>
2019-01-17 11:56:12 +0300

    feat(CRUD Articles): create models for the articles
    - allows for created articles to be saved in a database
    
    [Deliver #162948842]

---
+++ authors/apps/articles/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from authors.apps.authentication.models import User
+from django.template.defaultfilters import slugify
+from models.Cloudinary import CloudinaryField
+
+class Article(models.Model):
+    title = models.CharField(max_length=50, blank=False, unique=True)
+    body = models.TextField(blank=False, unique=True)
+    description = models.CharField(max_length=50, blank=False)
+    images = models.CloudinaryField(blank=False, default='')
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=50, blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Article, self).save(*args, **kwargs) 
