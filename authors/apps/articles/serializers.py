c206329ad0b799508ff8d57fc90247ad4f5568bc
KMaina <maina.ken0@gmail.com>
2019-01-18 12:18:18 +0300

    feat(CRUD Articles): add serializers for the Article model
    - Have a serializer for adding and retrieving posts
    - Have a serializer for updating and retrieving a single post
    - Have a serializer for deleting a post
    
    [Deliver #162948842]

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -1,18 +1,63 @@
+import re
+
 from rest_framework import serializers
 from .models import Article
 
-
 class ArticleSerializer(serializers.ModelSerializer):
+    """Serializers for creating and retrieving all articles"""
     # Field in the database corresponding to the User model
     author = serializers.SerializerMethodField()
     # Uploads an image to the Cloudinary servers
-    images = serializers.ImageField()
+    images = serializers.ImageField(default=None)
+    
 
     class Meta:
         model = Article
-        fields = ['title', 'slug', 'description', 'body', 'created', 'modified', 'images', 'author']
-        read_only_fields = ['created', 'modified', 'author', 'slug']
+        fields = ['title',
+                  'slug',
+                  'description',
+                  'body',
+                  'created',
+                  'modified',
+                  'images',
+                  'author']
+        read_only_fields = ['created',
+                            'modified',
+                            'author',
+                            'slug',
+                            'images',
+                            'is_dispayed']
 
     # Inserts the ID of the author of an article into the foreign key row
     def get_author(self,obj):
         return obj.author.id
+
+class GetArticleSerializer(serializers.ModelSerializer):
+    """Serializers for retrieving a single article and updating"""
+    # Field in the database corresponding to the User model
+    author = serializers.SerializerMethodField()
+    # Uploads an image to the Cloudinary servers
+    images = serializers.ImageField(default=None)
+
+    class Meta:
+        model = Article
+        fields = ['title',
+                  'description',
+                  'body',
+                  'modified',
+                  'images',
+                  'author',
+                  'slug']
+        read_only_fields = ['modified',
+                            'author',
+                            'title',
+                            'slug']
+
+    def get_author(self,obj):
+        return obj.author.id
+
+class DeleteArticleSerializer(serializers.ModelSerializer):
+    """Serializer for deleting an article"""
+    class Meta:
+        model = Article
+        fields = ['is_dispayed']
