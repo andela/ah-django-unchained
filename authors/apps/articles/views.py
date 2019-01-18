2762e51cea0df2572c830c519ab28a1a1ad6c557
KMaina <maina.ken0@gmail.com>
2019-01-18 12:27:10 +0300

    feat(CRUD Articles): add views for the articles app
    - add a view to post and fetch all articles
    - add a view to update an article
    - add a view to delete an article
    
    [Deliver #162948842]

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import status, generics, response
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.renderers import JSONRenderer
+from authors.apps.core.permissions import IsAuthorOrReadOnly
+from . import serializers
+from .serializers import ArticleSerializer, GetArticleSerializer, DeleteArticleSerializer
+from .models import Article
 
-# Create your views here.
+class ArticleAPIView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Article.objects.filter(is_dispayed=True)
+    serializer_class = ArticleSerializer
+    renderer_classes = (JSONRenderer,)
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class ArticleDetailsView(generics.RetrieveUpdateAPIView):
+    """This class handles the http GET and PUT requests."""
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Article.objects.filter(is_dispayed=True)
+    serializer_class = GetArticleSerializer
+    lookup_field = 'slug'
+
+class DeleteArticle(generics.UpdateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Article.objects.all()
+    serializer_class = DeleteArticleSerializer
+    lookup_field = 'slug'
