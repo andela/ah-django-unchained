3fb89c8128126432fe5f8321732f8b551231881a
kwame asiago <kwameasiago@gmail.com>
2019-01-19 17:50:41 +0300

    feat (Profile) Implement feedback
    - Make function easier to read
    
    [Delivers #162948841]

--- authors/apps/profiles/test/test_user_profile.py
+++ authors/apps/profiles/test/test_user_profile.py
@@ -16,25 +16,37 @@ class LoginTestCase(APITestCase):
                 "password": "@Qwerty12345"
                 }}
         self.profile_data = {
-            "first_name":"","last_name":"",
-            "gender":"M","bio":"","profile_image":None}
+            "first_name":"",
+            "last_name":"",
+            "gender":"M",
+            "bio":"",
+            "profile_image":None
+            }
 
     def test_get_profile(self):
         """Test get profile upon registrations"""
+        # register user
         register = self.client.post(self.signup_url,self.signup_data,format='json')
         self.assertEqual(register.status_code, status.HTTP_201_CREATED)
+        
+        # get user profile
         profile = self.client.get(reverse('profiles:get-profile', kwargs={'slug': 'johndoe'}),format='json')
         self.assertEqual(self.profile_data,profile.data)
     
     def test_post_profile(self):
-            """Test post profile upon registrations"""
-            self.profile_data['first_name'],self.profile_data['last_name'] = 'kwame', 'asiago'
-            self.profile_data['bio'] = 'some bio'
-            del self.profile_data['profile_image']
-            register = self.client.post(self.signup_url,self.signup_data,format='json')
-            self.assertEqual(register.status_code, status.HTTP_201_CREATED)
-            token  = register.data['token']
-            profile = self.client.put(reverse('profiles:post-profile', kwargs={'slug': 'johndoe'}),self.profile_data,
-            format='json',HTTP_AUTHORIZATION='token {}'.format(token))
-            self.profile_data['profile_image'] = None
-            self.assertEqual(self.profile_data,profile.data)
+        """Test post profile upon registrations"""
+        # override self.profile_data 
+        self.profile_data['first_name'],self.profile_data['last_name'] = 'kwame', 'asiago'
+        self.profile_data['bio'] = 'some bio'
+        del self.profile_data['profile_image']
+        
+        # sign up a user and get token
+        register = self.client.post(self.signup_url,self.signup_data,format='json')
+        self.assertEqual(register.status_code, status.HTTP_201_CREATED)
+        token  = register.data['token']
+            
+        # update profile
+        profile = self.client.put(reverse('profiles:post-profile', kwargs={'slug': 'johndoe'}),self.profile_data,
+        format='json',HTTP_AUTHORIZATION='token {}'.format(token))
+        self.profile_data['profile_image'] = None
+        self.assertEqual(self.profile_data,profile.data)
