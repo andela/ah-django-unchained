f9ea87a1bd550b71c033107364b338e5ad40e8e4
KMaina <maina.ken0@gmail.com>
2019-01-21 14:37:06 +0300

    feat(CRUD Articles): add changes in model
    - change name of deleted items to is_deleted
    - ensure code meets PEP8 standards
    
    [Deliver #162948842]

--- authors/apps/articles/models.py
+++ authors/apps/articles/models.py
@@ -3,17 +3,18 @@ from authors.apps.authentication.models import User
 
 from cloudinary.models import CloudinaryField
 
+
 class Article(models.Model):
     """
     This is the Article model that is used to handle CRUD on articles
     """
     title = models.CharField(max_length=200, blank=False)
-    body = models.TextField(blank=False, unique=True)
+    body = models.TextField(blank=False)
     description = models.CharField(max_length=50, blank=False)
-    images = CloudinaryField(blank=True, default='', null=True)
+    images = CloudinaryField(blank=True, default='No image', null=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=50, blank=False, unique=True)
-    is_displayed = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
     is_published = models.BooleanField(default=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
