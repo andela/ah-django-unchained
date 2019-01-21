2a99f4312f0711bfaee692b256190c74f2ad8704
KMaina <maina.ken0@gmail.com>
2019-01-21 14:41:11 +0300

    feat(CRUD Articles): ensure code meets PEP8 standards
    [Deliver #162948842]

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,30 +1,35 @@
 import random
 
 from django.template.defaultfilters import slugify
-from rest_framework import status, generics, response
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateAPIView, UpdateAPIView)
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
 from rest_framework.renderers import JSONRenderer
 from authors.apps.core.permissions import IsAuthorOrReadOnly
 
 from . import serializers
-from .serializers import ArticleSerializer, GetArticleSerializer, DeleteArticleSerializer
+from .serializers import (ArticleSerializer,
+                          GetArticleSerializer, DeleteArticleSerializer)
 from .models import Article
 
 
-class ArticleAPIView(generics.ListCreateAPIView):
+class ArticleAPIView(ListCreateAPIView):
     """Creates articles and retrieves all articles"""
     permission_classes = (IsAuthenticatedOrReadOnly,)
-    # Only fetch those articles whose 'is_displayed' field is True
-    queryset = Article.objects.filter(is_displayed=True)
+    # Only fetch those articles whose 'is_deleted' field is True
+    queryset = Article.objects.filter(is_deleted=False)
     serializer_class = ArticleSerializer
 
     def post(self, request):
         # Create unique slugs based on the article title
         slug = slugify(request.data['title'])
         new_slug = slug
-        random_num = random.randint(1,10000)
+        random_num = random.randint(1, 10000)
         # Checks if the slug name already exists
-        # If it does, it appends a randlomly generated number at the end of the slug
+        # If it does, append a randomly generated number at the end of the slug
         while Article.objects.filter(slug=new_slug).exists():
             new_slug = '{}-{}'.format(slug, random_num)
             random_num += 1
@@ -33,18 +38,19 @@ class ArticleAPIView(generics.ListCreateAPIView):
         serializer = self.serializer_class(data=article)
         if serializer.is_valid():
             serializer.save(author=self.request.user, slug=slug)
-            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
-        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class ArticleDetailsView(generics.RetrieveUpdateAPIView):
+
+class ArticleDetailsView(RetrieveUpdateAPIView):
     """This class handles the http GET and PUT requests."""
     permission_classes = (IsAuthorOrReadOnly,)
-    queryset = Article.objects.filter(is_displayed=True)
+    queryset = Article.objects.filter(is_deleted=False)
     serializer_class = GetArticleSerializer
-
     lookup_field = 'slug'
 
-class DeleteArticle(generics.UpdateAPIView):
+
+class DeleteArticle(UpdateAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Article.objects.all()
     serializer_class = DeleteArticleSerializer
