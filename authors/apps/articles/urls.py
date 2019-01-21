e87e8ae9e9f141ae8e080b1eaf2382b818656c4c
KMaina <maina.ken0@gmail.com>
2019-01-21 14:42:17 +0300

    feat(CRUD Articles): ensure code meets PEP8 standards
    [Deliver #162948842]

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 
 from .views import ArticleAPIView, ArticleDetailsView, DeleteArticle
 
-app_name="articles"
+app_name = "articles"
 urlpatterns = [
     path('articles/', ArticleAPIView.as_view(), name="articles-listcreate"),
-    path('articles/<slug:slug>/', ArticleDetailsView.as_view(), name="articles-retrieveupdate"),
-    path('articles/delete/<slug:slug>/', DeleteArticle.as_view(), name="articles-delete"),
+    path('articles/<slug:slug>/',
+         ArticleDetailsView.as_view(),
+         name="articles-retrieveupdate"),
+    path('articles/delete/<slug:slug>/',
+         DeleteArticle.as_view(),
+         name="articles-delete"),
 ]
