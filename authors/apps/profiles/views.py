aed82b24e39ef0e0906c52329da0727b1862c20d
kwame asiago <kwameasiago@gmail.com>
2019-01-24 20:46:35 +0300

    feat(profile) Make view readable
    - Create a response variable for update view
    
    [Delivers #162948841]

--- authors/apps/profiles/views.py
+++ authors/apps/profiles/views.py
@@ -17,9 +17,11 @@ class ProfileView(generics.RetrieveUpdateAPIView):
             user_instance = UserProfile.objects.select_related(
                 'user').get(user__username=username)
             if user_instance.user.username != request.user.username:
-                return Response({'error': 'You are not allowed to edit or delete this object'}, status.HTTP_403_FORBIDDEN)
+                data = {'error': 'You are not allowed to edit or delete this object'}
+                return Response(data, status.HTTP_403_FORBIDDEN)
         except ObjectDoesNotExist:
-            return Response({'error': 'Username {} not found'.format(username)}, status.HTTP_404_NOT_FOUND)
+            data = {'error': 'Username {} not found'.format(username)}
+            return Response(data, status.HTTP_404_NOT_FOUND)
         serializer = self.serializer_class(
             instance=user_instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
@@ -31,6 +33,7 @@ class ProfileView(generics.RetrieveUpdateAPIView):
             user_instance = UserProfile.objects.select_related(
                 'user').get(user__username=username)
         except ObjectDoesNotExist:
-            return Response({'error': 'Username {} not found'.format(username)}, status.HTTP_404_NOT_FOUND)
+            data = {'error': 'Username {} not found'.format(username)}
+            return Response(data , status.HTTP_404_NOT_FOUND)
         serializer = self.serializer_class(user_instance)
         return Response({'profile': serializer.data}, status.HTTP_200_OK)
