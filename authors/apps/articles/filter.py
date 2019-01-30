dbb4a61506eae6cf467466e98879b2fe111d300a
Margaret Chege <ciku@MacBook-Pro-4.local>
2019-01-30 15:28:08 +0300

    Add filter by tag

--- authors/apps/articles/filter.py
+++ authors/apps/articles/filter.py
@@ -9,11 +9,14 @@ class ArticleFilter(filters.FilterSet):
     author = filters.CharFilter(field_name='author__username',
                                 lookup_expr='exact')
     title = filters.CharFilter(field_name='title', lookup_expr='exact')
-    tagList = filters.CharFilter(field_name='tagList', lookup_expr='exact')
+    tags = filters.CharFilter(field_name='tagList', method='get_tags')
+
+    def get_tags(self, queryset, name, value):
+        return queryset.filter(tagList__name__icontains=value)
 
     class Meta:
         model = Article
-        fields = ['author', 'title', 'tagList']
+        fields = ['author', 'title', 'tags']
 
 
 class FilterArticles(ListAPIView):
